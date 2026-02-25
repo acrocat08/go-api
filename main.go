961443452042042d4ed0a774d8bf809a5c131b0f
asada0815 <asd0815electric@gmail.com>
2026-02-26 02:13:41 +0900

    init firebase

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
 
 func main() {
-	fmt.Println("hello")
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
 }
